75683ee9797ca0dd9825b3c56e3e782205ccf1dd
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2021-11-28 12:51:33 +0530

    GameManager script and GO added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ResourceSimulation.Manager
+{
+    public class GameManager : Singleton
+    {
+
+    }
+}
+
