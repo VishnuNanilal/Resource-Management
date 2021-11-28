bfbaf7017f7ef9a059fc9a42c1f21cfce9dc7ea2
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2021-11-28 13:16:54 +0530

    Persistent objects GO setup 1

--- Assets/Scripts/Helpers/Managers and Singletons/GameManager.cs
+++ Assets/Scripts/Helpers/Managers and Singletons/GameManager.cs
@@ -1,9 +1,18 @@
+using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 namespace ResourceSimulation.Core
 {
     public class GameManager : Singleton<GameManager>
     {
+        [SerializeField] List<GameObject> managers = new List<GameObject>();
+
+        private void Awake() {
+            foreach(GameObject manager in managers)
+                Instantiate(manager);
+        }
+
         public void QuitGame()
         {
             #if UNITY_EDITOR
