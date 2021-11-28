6a283002b30b74ef06bebcb68deafdfef72a8201
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2021-11-28 13:07:08 +0530

    QuitGame method added

--- Assets/Scripts/Helpers/Managers and Singletons/GameManager.cs
+++ Assets/Scripts/Helpers/Managers and Singletons/GameManager.cs
@@ -1,10 +1,17 @@
-
+using UnityEditor;
 
 namespace ResourceSimulation.Core
 {
     public class GameManager : Singleton<GameManager>
     {
-        
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
     }
 }
 
